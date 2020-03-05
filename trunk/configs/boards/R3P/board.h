62c7c4c32dec4cc70818566bc6268f46d0ce0ed0
ndwzy <55864304+ndwzy@users.noreply.github.com>
2020-03-05 19:15:00 +0800

    Add files via upload

---
+++ trunk/configs/boards/R3P/board.h
@@ -0,0 +1,33 @@
+/* R3P */
+
+#define BOARD_PID	"R3P"
+#define BOARD_NAME	"R3P"
+#define BOARD_DESC	"R3P Wireless Router"
+#define BOARD_VENDOR_NAME	"Xiaomi"
+#define BOARD_VENDOR_URL	"http://miwifi.com/"
+#define BOARD_MODEL_URL	"http://miwifi.com/"
+#define BOARD_BOOT_TIME	30
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_FN1
+#define BOARD_GPIO_BTN_RESET	18
+#undef BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#define  BOARD_GPIO_LED_POWER	8
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	14
+#define BOARD_GPIO_LED_USB  12
+#undef BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX 4
+#define BOARD_NUM_ANT_5G_RX 4
+#define BOARD_NUM_ANT_2G_TX 4
+#define BOARD_NUM_ANT_2G_RX 4
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_USB_PORT_SWAP	0
